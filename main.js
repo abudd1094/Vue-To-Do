47ae3fed2445964e3d7b7dce7057886b7496ffc5
Alec Budd <abudd1094@gmail.com>
2019-03-19 15:54:17 +0100

    working on addnote template

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-Vue.component('note', {
+Vue.component('addnote', {
+  props: {
+    noteTitle: this.noteTitle,
+    noteContent: this.noteContent
+  },
   template: `
-    <p>this is a note</p>
+    <div class="note">
+      <form @submit.prevent="onSubmit">
+        <label for="noteTitle">Title:</label>
+        <input type="text" id="noteTitle"><br>
+        <label for="noteContent">Content:</label>
+        <textarea id="noteContent" rows="1" columns="5" style="height: 56px"></textarea>
+        <input type="submit" value="Submit" class="button">
+      </form>  
+    </div>
   `
 })
 
